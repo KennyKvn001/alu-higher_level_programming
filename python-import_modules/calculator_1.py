2937b89c2252be38504d0bfe5bcb82f1b3071370
KenntKvn001 <k.mugisha@alustudent.com>
2023-07-10 08:11:29 -0700

    python-import_modules-02

--- python-import_modules/calculator_1.py
+++ python-import_modules/calculator_1.py
@@ -4,7 +4,7 @@ def add(a, b):  #addition function
     return (a + b)
 def sub(a, b):  #subtraction function
     return (a - b)
-def mult(a, b): #multiplication function
+def mul(a, b): #multiplication function
     return (a * b)
 def div(a, b):  #division function
     return(a // b)
