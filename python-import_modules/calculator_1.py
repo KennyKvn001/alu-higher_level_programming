64ff012e01b01136eb0449a6dc018a34f3cbb79b
KenntKvn001 <k.mugisha@alustudent.com>
2023-07-10 08:01:46 -0700

    python-import_modules-02

---
+++ python-import_modules/calculator_1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def add(a, b):  #addition function
+    return (a + b)
+def sub(a, b):  #subtraction function
+    return (a - b)
+def mult(a, b): #multiplication function
+    return (a * b)
+def div(a, b):  #division function
+    return(a // b)
