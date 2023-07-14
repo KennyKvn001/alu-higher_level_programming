1388152c0b39d40684d134621840d0285d90bf70
KenntKvn001 <k.mugisha@alustudent.com>
2023-07-14 10:46:25 -0700

    python-data_structures-02

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for idx in range(my_list, idx):
-        if idx == "-" and idx > my_list:
-            return None
+    if idx == "-" and idx > my_list:
+        return None
