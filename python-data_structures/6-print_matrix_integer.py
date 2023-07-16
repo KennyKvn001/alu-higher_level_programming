f671950807567413189e62114c34105cc227222b
Mugisha Kenny Kevin <127345283+KennyKvn001@users.noreply.github.com>
2023-07-16 09:06:38 +0200

    python-data_structures-07

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for c in range(len(matrix)):
         for r in range(len(matrix[c])):
             print("{:d}".format(matrix[c][r]), end="")
-            if r != len(matrix[c]):
+            if r != len(matrix[c]) - 1:
                 print(" ", end="")
         print("")
