de273717675fedd7cfe917c96dc03bff8a7d3c1c
KenntKvn001 <k.mugisha@alustudent.com>
2023-07-16 02:27:43 -0700

    python-data_structures-12

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,6 +3,5 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
     else:
-        for i in range(len(my_list)):
-            del idx[i]
-
+        for idx in range(len(my_list)):
+            del my_list[idx]
