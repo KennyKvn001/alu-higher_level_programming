7b90051a746118efc7e94835966299feccc7321f
KenntKvn001 <k.mugisha@alustudent.com>
2023-07-16 01:36:28 -0700

    python-data_structures-10

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,6 +3,6 @@ def max_integer(my_list=[]):
     if my_list == "":
         return None
     else:
-        for m in range(len(my_list)):
-            print("{:d}".format(m))
+        for max in my_list:
+            print("{:d}".format(max))
 
