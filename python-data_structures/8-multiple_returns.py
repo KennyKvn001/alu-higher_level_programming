1d05e398cd0ebba7ece08775bb03afdc4ece96c6
KenntKvn001 <k.mugisha@alustudent.com>
2023-07-16 01:08:54 -0700

    python-data_structures-09

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    for i in range(len(sentence)):
-        if sentence == "":
-            return (0, None)
-        else:
+    if sentence == "":
+        return (0, None)
+    else:
+        for i in range(len(sentence)):
             return (len(sentence), sentence[i])
