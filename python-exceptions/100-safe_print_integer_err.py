644723cdeaf3fdc4faf38757c4a22a7a1618b8ed
KenntKvn001 <k.mugisha@alustudent.com>
2023-07-20 02:29:49 -0700

    advanced-task-01

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,7 @@ def safe_print_integer_err(value):
     try:
         import sys
 
-        print("{}".format(value))
+        print("{:d}".format(value))
         return True
     except (Exception, TypeError, ValueError):
         sys.stderr.write(f"Error: {ex}\n")
