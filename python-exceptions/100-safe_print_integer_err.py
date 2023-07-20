d5bb8f92ec8289a18465103ced38d47a7098af23
Mugisha Kenny Kevin <127345283+KennyKvn001@users.noreply.github.com>
2023-07-20 15:46:25 +0200

    advanced-task-01

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
+import sys
 def safe_print_integer_err(value):
     
     try:
-        import sys
-
         print("{:d}".format(value))
         return True
-    except (Exception, TypeError, ValueError):
-        sys.stderr.write(f"Error: {ex}\n")
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1], file=sys.stderr))
         return False
