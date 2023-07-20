6322afd2044aa1702b22d262957bf16facf2fa5d
KenntKvn001 <k.mugisha@alustudent.com>
2023-07-20 08:35:45 -0700

    advanced-task-02

---
+++ python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    import sys
+
+
+    try:
+        safe = fct(*args)
+        return safe
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+
