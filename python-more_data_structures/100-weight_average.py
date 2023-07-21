9bdb9d009e9128e806edf9895f6ce172da0f9308
KenntKvn001 <k.mugisha@alustudent.com>
2023-07-21 01:45:40 -0700

    advanced-tsk-01

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-from functools import reduce
 def weight_average(my_list=[]):
-    if not isinstance(my_list, list) or len(my_list):
+    if not isinstance(my_list, list) or len(my_list) == 0:
         return 0
+
     avg = 0
     size = 0
-    for tup in my_list:
-        avg += (tup[0] * tup[1])
-        size += tup[1]
-    return (avg / size)
+    for x in my_list:
+        avg += (x[0] * x[1])
+        size += x[1]
+    return avg / size
