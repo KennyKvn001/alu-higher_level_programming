e1917ac28e5decc9cbff0ba08d5581f59ae4b0cc
Mugisha Kenny Kevin <127345283+KennyKvn001@users.noreply.github.com>
2023-07-21 11:10:47 +0200

    advanced-tsk-01

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -3,10 +3,6 @@ def weight_average(my_list=[]):
     if not isinstance(my_list, list) or len(my_list) == 0:
         return 0
 
-<<<<<<< HEAD
-=======
-    
->>>>>>> 7942fb7647ee3e9c1019786669645d7488a49fb2
     avg = 0
     size = 0
     for x in my_list:
