c4c0edb3dc78c9e8813ef5a6ac70b9ac1473a7d2
Mugisha Kenny Kevin <127345283+KennyKvn001@users.noreply.github.com>
2023-07-25 11:41:56 +0200

    python-classes-05

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Instantiation with optional size"""
     def __init__(self, size=0):
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
@@ -18,6 +18,7 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
+            self.__size = value
 
     def area(self):
         return (self.__size * self.__size)
