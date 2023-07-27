bc59d6ff65c363b275dedf70462517077b692d7d
Mugisha Kenny Kevin <127345283+KennyKvn001@users.noreply.github.com>
2023-07-27 19:40:56 +0200

    python-inheritance-06

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -2,11 +2,6 @@
 """Defines an empty class BaseGeometry."""
 
 
-<<<<<<< HEAD
 class BaseGeometry:
     """Represent base geometry."""
-=======
-class BaseGoemetry:
-    """represents an empty base geometry"""
->>>>>>> 18fc75cf4c153e05267e3a026d63f6e07cc703fe
     pass
