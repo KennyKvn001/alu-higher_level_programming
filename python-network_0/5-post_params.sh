66b7498f85ce35f49c0ce1b916722df15eac391e
Mugisha Kenny Kevin <127345283+KennyKvn001@users.noreply.github.com>
2023-08-04 15:26:32 +0200

    python-network-06

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get the byte size of the HTTP response header for a given URL.
-curl -s "$1" | wc -c
+# Bash scripts that sends a POST request to a given URL.
+curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
