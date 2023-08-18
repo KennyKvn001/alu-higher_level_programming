fd171774db5ebd50065bda6086093caaf1ec8c39
Mugisha Kenny Kevin <127345283+KennyKvn001@users.noreply.github.com>
2023-08-18 20:37:01 +0200

    python-ORM-14

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -6,19 +6,26 @@ Usage: ./13-model_state_delete_a.py <mysql username> /
                                     <mysql password> /
                                     <database name>"""
 
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import State
-
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    engine = create_engine(
+        f"mysql+mysqldb://{mysql_username}:{mysql_password}
+        @localhost:3306/{database_name}",
+        pool_pre_ping=True
+    )
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State):
-        if "a" in state.name:
-            session.delete(state)
+    states_to_delete = session.query(State)
+    .filter(State.name.like('%a%')).all()
+    
+    for state in states_to_delete:
+        session.delete(state)
+
     session.commit()
+  
