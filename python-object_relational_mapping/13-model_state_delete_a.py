338c61ee78897619ec0597c31e9eec26b6328e4d
Mugisha Kenny Kevin <127345283+KennyKvn001@users.noreply.github.com>
2023-08-18 22:29:49 +0200

    python-ORM-14

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
+""" this script deletes an object in the table 'states'
+in the database hbtn_0e_6_usa
 """
-Deletes all State objects with a name containing
-the letter a from the database hbtn_0e_6_usa.
-Usage: ./13-model_state_delete_a.py <mysql username> \
-                                    <mysql password> \
-                                    <database name>
-"""
-
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
